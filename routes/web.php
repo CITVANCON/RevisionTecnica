de919ea450a82c630332683267ad7bd8a8338c81
CITVANCON <citvancon@gmail.com>
2026-03-02 11:13:21 -0500

    juntar pagina web

--- routes/web.php
+++ routes/web.php
@@ -16,21 +16,48 @@ use App\Livewire\RRHH\ListaPlanilla;
 use App\Livewire\RRHH\MisPlanillas;
 use App\Livewire\SubirFotografias;
 use App\Livewire\Usuarios;
+use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*Route::get('/', function () {
     return view('welcome');
 });*/
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return redirect()->to('/login');
+});*/
+
+Route::get('/', function () {
+    return view('index');
 });
+
+Route::get('index', function () {
+    return view('index');
+});
+
+Route::get('about', function () {
+    return view('about');
+});
+Route::get('service', function () {
+    return view('service');
+});
+Route::get('contact', function () {
+    return view('contact');
+});
+
+//Route::get('/asistencia', MarcadoAsistencia::class)->name('marcar');
 
 Route::get('phpmyinfo', function () {
     phpinfo();
 })->name('phpmyinfo');
 
-Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified'])
+RateLimiter::for('livewire', function (Request $request) {
+    return Limit::perMinute(10)->by($request->ip()); // Máx 10 solicitudes por minuto por IP
+});
+
+/*Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified'])
     ->group(function () {
         Route::get('/', function () {
             return view('dashboard');
@@ -39,75 +66,66 @@ Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified']
         //Route::get('/altavehiculo', Prueba::class)->name('altavehiculo');
         Route::get('/altavehiculo', Prueba::class);
     });
+*/
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified',])
+    ->group(function () {
 
-
-    //Ruta para alta de vehiculo
-    Route::get('/altavehiculo', Prueba::class)->name('altavehiculo');
-
-    // Rutas para linea de inspeccion
-    Route::get('/lineainspeccion', Linea::class)->name('lineainspeccion');
-    Route::get('/lineainspeccion/{idPropuesta}', EditarLineaInspeccion::class)->name('editar-lineainspeccion');
-
-    // Ruta para subir fotografias a una propuesta
-    Route::get('/subir-fotografias', SubirFotografias::class)->name('subirFotografias');
-
-    //Ruta para admin inspecciones
-    Route::get('/Admin-inspecciones', AdministracionInspecciones::class)->name('AdminInspecciones');
-
-    Route::get('/Expedientes', Expedientes::class)->name('expedientes');
+        Route::get('/dashboard', function () {
+            return view('dashboard');
+        })->name('dashboard');
 
 
-    Route::get('/rrhh/contratos', Contratos::class)->name('rrhh.contratos'); //->middleware('can:rrhh.contratos')
-    Route::get('/rrhh/vacaciones/contrato/{idContrato}', GestionarVacaciones::class)->name('rrhh.vacaciones.index');
-    Route::get('/rrhh/documentos/{id?}', GestionDocumentos::class)->name('rrhh.documentos');
-    Route::get('/rrhh/planillas', ListaPlanilla::class)->name('rrhh.planillas');
-    Route::get('/rrhh/mis-planillas', MisPlanillas::class)->name('rrhh.mis-planillas');
+        //Ruta para alta de vehiculo
+        Route::get('/altavehiculo', Prueba::class)->name('altavehiculo');
+
+        // Rutas para linea de inspeccion
+        Route::get('/lineainspeccion', Linea::class)->name('lineainspeccion');
+        Route::get('/lineainspeccion/{idPropuesta}', EditarLineaInspeccion::class)->name('editar-lineainspeccion');
+
+        // Ruta para subir fotografias a una propuesta
+        Route::get('/subir-fotografias', SubirFotografias::class)->name('subirFotografias');
+
+        //Ruta para admin inspecciones
+        Route::get('/Admin-inspecciones', AdministracionInspecciones::class)->name('AdminInspecciones');
+
+        Route::get('/Expedientes', Expedientes::class)->name('expedientes');
 
 
-    // Rutas para usuarios y roles
-    Route::get('/Usuarios', Usuarios::class)->middleware('can:usuarios')->name('usuarios');
-    Route::get('/Roles', Roles::class)->middleware('can:usuarios.roles')->name('usuarios.roles');
-    Route::get('/Permisos', Permisos::class)->middleware('can:usuarios.permisos')->name('usuarios.permisos');
+        Route::get('/rrhh/contratos', Contratos::class)->name('rrhh.contratos'); //->middleware('can:rrhh.contratos')
+        Route::get('/rrhh/vacaciones/contrato/{idContrato}', GestionarVacaciones::class)->name('rrhh.vacaciones.index');
+        Route::get('/rrhh/documentos/{id?}', GestionDocumentos::class)->name('rrhh.documentos');
+        Route::get('/rrhh/planillas', ListaPlanilla::class)->name('rrhh.planillas');
+        Route::get('/rrhh/mis-planillas', MisPlanillas::class)->name('rrhh.mis-planillas');
 
 
-    //RUTAS PARA STREAM Y DESCARGA DE PDFS
-    Route::controller(PdfController::class)->group(function () {
+        // Rutas para usuarios y roles
+        Route::get('/Usuarios', Usuarios::class)->middleware('can:usuarios')->name('usuarios');
+        Route::get('/Roles', Roles::class)->middleware('can:usuarios.roles')->name('usuarios.roles');
+        Route::get('/Permisos', Permisos::class)->middleware('can:usuarios.permisos')->name('usuarios.permisos');
 
-        //Rutas para ver certificado anual GNV
-        Route::get('/inspeccion/{id}', 'generaPdfInspeccion')->name("inspeccion");
-        //Rutas para descargar certificado anual GNV
-        Route::get('/inspeccion/{id}/descargar', 'descargarPdfInspeccion')->name("descargarInspeccion");
 
-        Route::get('/rrhh/contrato/{id}/pdf', 'generarContrato')->name('rrhh.contrato.pdf');
+        //RUTAS PARA STREAM Y DESCARGA DE PDFS
+        Route::controller(PdfController::class)->group(function () {
 
+            //Rutas para ver certificado anual GNV
+            Route::get('/inspeccion/{id}', 'generaPdfInspeccion')->name("inspeccion");
+            //Rutas para descargar certificado anual GNV
+            Route::get('/inspeccion/{id}/descargar', 'descargarPdfInspeccion')->name("descargarInspeccion");
+
+            Route::get('/rrhh/contrato/{id}/pdf', 'generarContrato')->name('rrhh.contrato.pdf');
+        });
+
+
+        //Rutas para procesos de INTEROPERABILIDAD (soap)
+        Route::get('/mtc/iniciar', [MTCSoapController::class, 'iniciarOperacion']);
+        //Route::get('/mtc/consultar-vehiculo', [MTCSoapController::class, 'consultarVehiculo']);
+        Route::get('/mtc/registrar-poliza', [MTCSoapController::class, 'registrarPoliza']);
+        Route::get('/mtc/registrar-resultado', [MTCSoapController::class, 'registrarResultadoRevision']);
+        Route::get('/mtc/actualizar-poliza', [MTCSoapController::class, 'actualizarPoliza']);
+        Route::get('/mtc/anular-certificado', [MTCSoapController::class, 'anularCertificado']);
+        Route::get('/mtc/cerrar-operaciones', [MTCSoapController::class, 'cerrarOperaciones']);
+
+        // Llamar al método del controlador desde el componente usando Http::post(...)
+        Route::post('/mtc/consultar-vehiculo', [MTCSoapController::class, 'consultarVehiculo'])->name('mtc.consultarVehiculo');
     });
-
-
-    //Rutas para procesos de INTEROPERABILIDAD (soap)
-    Route::get('/mtc/iniciar', [MTCSoapController::class, 'iniciarOperacion']);
-    //Route::get('/mtc/consultar-vehiculo', [MTCSoapController::class, 'consultarVehiculo']);
-    Route::get('/mtc/registrar-poliza', [MTCSoapController::class, 'registrarPoliza']);
-    Route::get('/mtc/registrar-resultado', [MTCSoapController::class, 'registrarResultadoRevision']);
-    Route::get('/mtc/actualizar-poliza', [MTCSoapController::class, 'actualizarPoliza']);
-    Route::get('/mtc/anular-certificado', [MTCSoapController::class, 'anularCertificado']);
-    Route::get('/mtc/cerrar-operaciones', [MTCSoapController::class, 'cerrarOperaciones']);
-
-    // Llamar al método del controlador desde el componente usando Http::post(...)
-    Route::post('/mtc/consultar-vehiculo', [MTCSoapController::class, 'consultarVehiculo'])->name('mtc.consultarVehiculo');
-
-
-
-
-
-
-
-});
